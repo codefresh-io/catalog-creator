e025c4fc8352a8537b332cb611363f71a9f8f1cb
verchol <verchol@gmail.com>
2018-12-19 01:26:44 +0200

    fix

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo on
 echo 'git configure'
-#sh git-configure.sh  $USER_EMAIL $USERNAME $PASSWORD 
+sh git-configure.sh  $USER_EMAIL $USERNAME $PASSWORD 
 echo 'create catalog' , current branch $CF_BRANCH
 sh plugin-logic.sh "$repo" "$indexPath" "$masterBranch"  $CF_BRANCH
