dd9446a377e60a587b34908ab22d215745e71ee2
Anton Weiss <anton@otomato.link>
2019-02-11 12:08:46 +0200

    fix git configuration script

--- git-configure.sh
+++ git-configure.sh
@@ -1,6 +1,7 @@
 echo git configure with params $1 $2 $3
+USER_EMAIL=$1
 USERNAME=$2 
 PASSWORD=$3
 git config --global credential.helper "/bin/sh -c 'echo username=$USERNAME;echo password=$PASSWORD'"
-git config --global user.email $USERNAME
-git config --global user.name  $PASSWORD
+git config --global user.email "$USER_EMAIL"
+git config --global user.name  $USERNAME
